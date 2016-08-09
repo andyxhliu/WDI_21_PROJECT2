0eb31fef9bf60a66990a1f301569494891fbe14a
andyxhliu <andyxhliu@outlook.com>
2016-08-09 15:20:18 +0100

    Tags added to Group and Event

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,7 @@ class Group < ApplicationRecord
   has_and_belongs_to_many :users
   has_many :events
   has_many :comments
+  has_many :tags, through: :events
 
   mount_uploader :group_image, GroupImageUploader
 
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,3 +1,4 @@
 class Tag < ApplicationRecord 
   has_and_belongs_to_many :events
+  has_many :groups, through: :events
 end
